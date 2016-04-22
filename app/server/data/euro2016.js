c541858a63ce6e20cd6279ab19e824e26852d354
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-04-22 13:21:13 +0200

    correct dates

--- app/server/data/euro2016.js
+++ app/server/data/euro2016.js
@@ -126,179 +126,184 @@ Meteor.startup(() => {
   const matches = [{
     team1: 'euro2016_France',
     team2: 'euro2016_Romania',
-    date: Date.UTC(2016,06,10,21,00),
+    date: Date.UTC(2016,5,10,21,00),
     stadium: 'Stade de France, Saint-Denis'
   },{
     team1: 'euro2016_Albania',
     team2: 'euro2016_Switzerland',
-    date: Date.UTC(2016,06,11,15,00),
+    date: Date.UTC(2016,5,11,15,00),
     stadium: 'Stade Bollaert-Delelis, Lens Agglo'
   },{
     team1: 'euro2016_Wales',
     team2: 'euro2016_Slovakia',
-    date: Date.UTC(2016,06,11,18,00),
+    date: Date.UTC(2016,5,11,18,00),
     stadium: 'Stade de Bordeaux, Bordeaux'
   },{
     team1: 'euro2016_England',
     team2: 'euro2016_Russia',
-    date: Date.UTC(2016,06,11,21,00),
+    date: Date.UTC(2016,5,11,21,00),
     stadium: 'Stade Vélodrome, Marseille'
   },{
     team1: 'euro2016_Turkey',
     team2: 'euro2016_Croatia',
-    date: Date.UTC(2016,06,12,15,00),
+    date: Date.UTC(2016,5,12,15,00),
     stadium: 'Parc de Princes, Paris'
   },{
     team1: 'euro2016_Poland',
     team2: 'euro2016_Northern_Ireland',
-    date: Date.UTC(2016,06,12,18,00),
+    date: Date.UTC(2016,5,12,18,00),
     stadium: 'Stade de Nice, Nice'
   },{
     team1: 'euro2016_Germany',
     team2: 'euro2016_Ukraine',
-    date: Date.UTC(2016,06,12,21,00),
+    date: Date.UTC(2016,5,12,21,00),
     stadium: 'Stade Pierre Mauroy, Lille Métropole'
   },{
     team1: 'euro2016_Spain',
     team2: 'euro2016_Czech_Republic',
-    date: Date.UTC(2016,06,13,15,00),
+    date: Date.UTC(2016,5,13,15,00),
     stadium: 'Stade de Toulouse, Toulouse'
   },{
     team1: 'euro2016_Republic_of_Ireland',
     team2: 'euro2016_Sweden',
-    date: Date.UTC(2016,06,13,18,00),
+    date: Date.UTC(2016,5,13,18,00),
     stadium: 'Stade de France, Saint-Denis'
   },{
     team1: 'euro2016_Belgium',
     team2: 'euro2016_Italy',
-    date: Date.UTC(2016,06,13,21,00),
+    date: Date.UTC(2016,5,13,21,00),
     stadium: 'Stade de Lyon, Lyon'
   },{
     team1: 'euro2016_Austria',
     team2: 'euro2016_Hungary',
-    date: Date.UTC(2016,06,14,18,00),
+    date: Date.UTC(2016,5,14,18,00),
     stadium: 'Stade de Bordeaux, Bordeaux'
   },{
     team1: 'euro2016_Portugal',
     team2: 'euro2016_Iceland',
-    date: Date.UTC(2016,06,14,21,00),
+    date: Date.UTC(2016,5,14,21,00),
     stadium: 'Stade Geoffroy Guichard, Saint-Etienne'
   },
   {
     team1: 'euro2016_Russia',
     team2: 'euro2016_Slovakia',
-    date: Date.UTC(2016,06,15,15,00),
+    date: Date.UTC(2016,5,15,15,00),
     stadium: 'Stade Pierre Mauroy, Lille Métropole'
   },{
     team1: 'euro2016_Romania',
     team2: 'euro2016_Switzerland',
-    date: Date.UTC(2016,06,15,18,00),
+    date: Date.UTC(2016,5,15,18,00),
     stadium: 'Parc des Princes, Paris'
   },{
     team1: 'euro2016_France',
     team2: 'euro2016_Albania',
-    date: Date.UTC(2016,06,15,21,00),
+    date: Date.UTC(2016,5,15,21,00),
     stadium: 'Stade Vélodrome, Marseille'
   },{
     team1: 'euro2016_England',
     team2: 'euro2016_Wales',
-    date: Date.UTC(2016,06,16,15,00),
+    date: Date.UTC(2016,5,16,15,00),
     stadium: 'Stade Bollaert-Delelis, Lens Agglo'
   },{
     team1: 'euro2016_Ukraine',
     team2: 'euro2016_Northern_Ireland',
-    date: Date.UTC(2016,06,16,18,00),
+    date: Date.UTC(2016,5,16,18,00),
     stadium: 'Stade de Lyon, Lyon'
   },{
     team1: 'euro2016_Germany',
     team2: 'euro2016_Poland',
-    date: Date.UTC(2016,06,16,21,00),
+    date: Date.UTC(2016,5,16,21,00),
     stadium: 'Stade de France, Saint-Denis'
   },{
     team1: 'euro2016_Italy',
     team2: 'euro2016_Sweden',
-    date: Date.UTC(2016,06,17,15,00),
+    date: Date.UTC(2016,5,17,15,00),
     stadium: 'Stade de Toulouse, Toulouse'
   },{
     team1: 'euro2016_Czech_Republic',
     team2: 'euro2016_Croatia',
-    date: Date.UTC(2016,06,17,18,00),
+    date: Date.UTC(2016,5,17,18,00),
     stadium: 'Stade Geoffroy Guichard, Saint-Etienne'
   },{
     team1: 'euro2016_Spain',
     team2: 'euro2016_Turkey',
-    date: Date.UTC(2016,06,17,21,00),
+    date: Date.UTC(2016,5,17,21,00),
     stadium: 'Stade de Nice, Nice'
   },{
     team1: 'euro2016_Belgium',
     team2: 'euro2016_Republic_of_Ireland',
-    date: Date.UTC(2016,06,18,15,00),
+    date: Date.UTC(2016,5,18,15,00),
     stadium: 'Stade de Bordeaux, Bordeaux'
   },{
     team1: 'euro2016_Iceland',
     team2: 'euro2016_Hungary',
-    date: Date.UTC(2016,06,18,18,00),
+    date: Date.UTC(2016,5,18,18,00),
     stadium: 'Stade Vélodrome, Marseille'
   },{
     team1: 'euro2016_Portugal',
     team2: 'euro2016_Austria',
-    date: Date.UTC(2016,06,18,21,00),
+    date: Date.UTC(2016,5,18,21,00),
     stadium: 'Stade des Princes, Paris'
   },
   {
     team1: 'euro2016_Romania',
     team2: 'euro2016_Albania',
-    date: Date.UTC(2016,06,19,21,00),
+    date: Date.UTC(2016,5,19,21,00),
     stadium: 'Stade de Lyon, Lyon'
   },{
     team1: 'euro2016_Switzerland',
     team2: 'euro2016_France',
-    date: Date.UTC(2016,06,19,21,00),
+    date: Date.UTC(2016,5,19,21,00),
     stadium: 'Stade Pierre Mauroy, Lille Métropole'
   },{
     team1: 'euro2016_Russia',
     team2: 'euro2016_Wales',
-    date: Date.UTC(2016,06,20,21,00),
+    date: Date.UTC(2016,5,20,21,00),
     stadium: 'Stade de Toulouse, Toulouse'
+  },{
+    team1: 'euro2016_Slovakia',
+    team2: 'euro2016_England',
+    date: Date.UTC(2016,5,20,21,00),
+    stadium: 'Stade Geoffroy Guichard, Saint-Etienne'
   },{
     team1: 'euro2016_Ukraine',
     team2: 'euro2016_Poland',
-    date: Date.UTC(2016,06,21,18,00),
+    date: Date.UTC(2016,5,21,18,00),
     stadium: 'Stade Vélodrome, Marseille'
   },{
     team1: 'euro2016_Northern_Ireland',
     team2: 'euro2016_Germany',
-    date: Date.UTC(2016,06,21,18,00),
+    date: Date.UTC(2016,5,21,18,00),
     stadium: 'Stade des Princes, Paris'
   },{
     team1: 'euro2016_Czech_Republic',
     team2: 'euro2016_Turkey',
-    date: Date.UTC(2016,06,21,21,00),
+    date: Date.UTC(2016,5,21,21,00),
     stadium: 'Stade Bollaert-Delelis, Lens Agglo'
   },{
     team1: 'euro2016_Croatia',
     team2: 'euro2016_Spain',
-    date: Date.UTC(2016,06,21,21,00),
+    date: Date.UTC(2016,5,21,21,00),
     stadium: 'Stade de Bordeaux, Bordeaux'
   },{
     team1: 'euro2016_Iceland',
     team2: 'euro2016_Austria',
-    date: Date.UTC(2016,06,22,18,00),
+    date: Date.UTC(2016,5,22,18,00),
     stadium: 'Stade de France, Saint-Denis'
   },{
     team1: 'euro2016_Hungary',
     team2: 'euro2016_Portugal',
-    date: Date.UTC(2016,06,22,18,00),
+    date: Date.UTC(2016,5,22,18,00),
     stadium: 'Stade de Lyon, Lyon'
   },{
     team1: 'euro2016_Italy',
     team2: 'euro2016_Republic_of_Ireland',
-    date: Date.UTC(2016,06,22,21,00),
+    date: Date.UTC(2016,5,22,21,00),
     stadium: 'Stade Pierre Mauroy, Lille Métropole'
   },{
     team1: 'euro2016_Sweden',
     team2: 'euro2016_Belgium',
-    date: Date.UTC(2016,06,22,21,00),
+    date: Date.UTC(2016,5,22,21,00),
     stadium: 'Stade de Nice, Nice'
   }];
   matches.forEach(match => {
