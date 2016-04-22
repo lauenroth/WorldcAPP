8c013b763fbe57c67ce8398956732621b15e0b2a
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-04-22 13:22:56 +0200

    UI for placing a bet

--- app/client/templates/bet/bet.js
+++ app/client/templates/bet/bet.js
@@ -1,7 +1,34 @@
+Template.Bet.setBet = function(e, target) {
+  e.preventDefault();
+  const chosen = $(e.currentTarget).blur().html();
+
+  if (chosen === '-') {
+    const newValue = parseInt(target.html()) - 1 || 0;
+    if (newValue >= 0) {
+      target.html(newValue);
+    }
+  }
+  else if (chosen === '+') {
+    const newValue = parseInt(target.html()) + 1 || 0;
+    target.html(newValue);
+  }
+  else {
+    target.html(chosen);
+  }
+};
+
 /*****************************************************************************/
 /* Bet: Event Handlers */
 /*****************************************************************************/
 Template.Bet.events({
+
+  'click .set-bet1 a': function(e) {
+    Template.Bet.setBet(e, $('.bet-team1'));
+  },
+
+  'click .set-bet2 a': function(e) {
+    Template.Bet.setBet(e, $('.bet-team2'));
+  },
 });
 
 /*****************************************************************************/
