7c6f9723e76a8091724f89d65b04597f8457cc6f
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-04-23 23:37:43 +0200

    Group matches by day

--- app/client/templates/matches/matches.js
+++ app/client/templates/matches/matches.js
@@ -10,15 +10,39 @@ Template.Matches.events({
 Template.Matches.helpers({
 
   groupMatches: function() {
+    const groupMatches = [];
     const matches = Matches.find({
       tournament: Session.get('tournament'),
       stage: 'group'
     }).fetch();
+    let currentDayMatches = [];
+    let currentDay = false;
     matches.forEach(match => {
-      match.team1name = Teams.findOne({_id: match.team1}).name;
+      const team1 = Teams.findOne({_id: match.team1});
+      match.team1name = team1.name;
       match.team2name = Teams.findOne({_id: match.team2}).name;
+
+      match.group = Groups.findOne({_id: team1.group}).name;
+
+      // same day as the current day?
+      const matchDay = moment.utc(match.date).format('DD/MM/YYYY');
+      if (!currentDay) {
+        currentDay = matchDay;
+      } else if (currentDay !== matchDay) {
+        groupMatches.push({
+          day: currentDay,
+          matches: currentDayMatches
+        });
+        currentDayMatches = [];
+        currentDay = matchDay;
+      }
+      currentDayMatches.push(match);
     });
-    return matches;
+    groupMatches.push({
+      day: currentDay,
+      matches: currentDayMatches
+    });
+    return groupMatches;
   },
 
 });
