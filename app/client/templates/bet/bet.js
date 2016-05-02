8aab8b503ef75214c720d926a34d15f254e0db90
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-05-02 16:24:37 +0200

    Set bet for a match and go to the next/prev match

--- app/client/templates/bet/bet.js
+++ app/client/templates/bet/bet.js
@@ -8,21 +8,31 @@ Template.Bet.setBet = function(e, target) {
   target.html(chosen);
 };
 
-Template.Bet.saveBet = function(match) {
+Template.Bet.saveBet = function() {
+  const match = Session.get('currentMatch');
   const score1 = $('.bet-team1').html();
   const score2 = $('.bet-team2').html();
-  let bet = Bets.findOne({match: match._id});
-  if (!bet) {
-    Bets.insert({
-      match: match._id,
-      score1: score1,
-      score2: score2,
-      userId: Meteor.userId(),
-    });
-  } else {
-    bet.score1 = score1;
-    bet.score2 = score2;
+  if (score1.length && score2.length) {
+    const bet = Bets.findOne({match: match._id});
+    if (!bet) {
+      Bets.insert({
+        match: match._id,
+        score1: score1,
+        score2: score2,
+        userId: Meteor.userId(),
+      });
+    } else {
+      Bets.update({_id: bet._id}, {$set: {score1: score1, score2: score2}});
+    }
   }
+};
+
+Template.Bet.setMatch = function(data) {
+  data.match = data.matches[data.currentMatchIndex];
+  data.match.team1name = Teams.findOne({_id: data.match.team1}).name;
+  data.match.team2name = Teams.findOne({_id: data.match.team2}).name;
+  data.match.bet = Bets.findOne({match: data.match._id, userId: Meteor.userId()});
+  Session.set('currentMatch', data.match);
 };
 
 /*****************************************************************************/
@@ -40,12 +50,22 @@ Template.Bet.events({
 
   'click .prev': function(e) {
     e.preventDefault();
-    Template.Bet.saveBet(this.match);
+    Template.Bet.saveBet();
+
+    if (this.currentMatchIndex > 0) {
+      this.currentMatchIndex--;
+      Template.Bet.setMatch(this);
+    }
   },
 
   'click .next': function(e) {
     e.preventDefault();
-    Template.Bet.saveBet(this.match);
+    Template.Bet.saveBet();
+
+    if (this.matches.length > this.currentMatchIndex + 2) {
+      this.currentMatchIndex++;
+      Template.Bet.setMatch(this);
+    }
   },
 });
 
@@ -60,6 +80,21 @@ Template.Bet.helpers({
     }
   },
 
+  currentMatch: function() {
+    return Session.get('currentMatch');
+  },
+
+  hasPrevMatch: function() {
+    Session.get('currentMatch');
+    return this.currentMatchIndex > 0;
+  },
+
+  hasNextMatch: function() {
+    Session.get('currentMatch');
+    console.log(this.currentMatchIndex, this.matches.length);
+    return this.currentMatchIndex < this.matches.length - 2;
+  },
+
 });
 
 /*****************************************************************************/
@@ -70,6 +105,17 @@ Template.Bet.onCreated(function () {
 
 Template.Bet.onRendered(function () {
   Session.set('menuItem', 'bet');
+
+  if (!this.data || !this.data.match) {
+    Router.go('bets');
+  } else {
+    Session.set('currentMatch', this.data.match);
+    this.data.matches.forEach((matchTmp, index) => {
+      if (matchTmp._id === this.data.match._id) {
+        this.data.currentMatchIndex = index;
+      }
+    });
+  }
 });
 
 Template.Bet.onDestroyed(function () {
