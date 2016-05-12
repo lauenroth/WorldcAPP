26390c293a3950a160d3f079e54d14a4c23740b0
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-05-12 08:32:17 +0200

    Added knock-out stage

--- app/client/templates/standings/standings.js
+++ app/client/templates/standings/standings.js
@@ -10,12 +10,22 @@ Template.Standings.events({
 Template.Standings.helpers({
 
   groups: function() {
-    let groups = Groups.find().fetch();
+    let groups = Groups.find({}, {sort: {name: 1}}).fetch();
     groups.forEach(group => {
-      group.teams = Teams.find({group: group._id}).fetch();
+      group.teams = Teams.find({group: group._id}, {sort: {points: -1}}).fetch();
     });
     return groups;
   },
+
+  goalDifference: function(team) {
+    const difference = team.goalsFor - team.goalsAgainst;
+    return difference > 0 ? '+' + difference : difference;
+  },
+
+  isGroupStage: function() {
+    return Session.get('stage') === 'group';
+  },
+
 });
 
 /*****************************************************************************/
@@ -26,6 +36,10 @@ Template.Standings.onCreated(function () {
 
 Template.Standings.onRendered(function () {
   Session.set('menuItem', 'standings');
+
+  $('.pages-wrapper').dragend({
+    pageClass: 'page',
+  });
 });
 
 Template.Standings.onDestroyed(function () {
