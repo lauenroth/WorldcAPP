7e2b3f6d111d1b8c171898d6cbfabb1e72d62026
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-06-02 22:17:43 +0200

    better standings

--- app/client/templates/standings/standings.js
+++ app/client/templates/standings/standings.js
@@ -1,3 +1,17 @@
+Template.Standings.getMatchesForStage = function(stage) {
+  let matches = Matches.find({stage: stage}, {sort: {date: 1}}).fetch();
+  matches.forEach(match => {
+    if (match.team1 !== 'tbd') {
+      match.team1 = Teams.findOne({_id: match.team1});
+      match.team2 = Teams.findOne({_id: match.team2});
+    } else {
+      match.team1 = {name: 'tbd'};
+      match.team2 = {name: 'tbd'};
+    }
+  });
+  return matches;
+};
+
 /*****************************************************************************/
 /* Standings: Event Handlers */
 /*****************************************************************************/
@@ -12,13 +26,29 @@ Template.Standings.helpers({
   groups: function() {
     let groups = Groups.find({}, {sort: {name: 1}}).fetch();
     groups.forEach(group => {
-      group.teams = Teams.find({group: group._id}, {sort: {points: -1}}).fetch();
+      group.teams = Teams.find({group: group._id}, {sort: {points: -1, goalDifference: -1}}).fetch();
     });
     return groups;
   },
 
+  roundOf16: function() {
+    return Template.Standings.getMatchesForStage('roundOf16');
+  },
+
+  quarterFinals: function() {
+    return Template.Standings.getMatchesForStage('quarterFinals');
+  },
+
+  semiFinals: function() {
+    return Template.Standings.getMatchesForStage('semiFinals');
+  },
+
+  final: function() {
+    return Template.Standings.getMatchesForStage('final');
+  },
+
   goalDifference: function(team) {
-    const difference = team.goalsFor - team.goalsAgainst;
+    const difference = team.goalDifference;
     return difference > 0 ? '+' + difference : difference;
   },
 
@@ -37,10 +67,9 @@ Template.Standings.onCreated(function () {
 Template.Standings.onRendered(function () {
   Session.set('menuItem', 'standings');
 
-  $('.pages-wrapper').dragend({
-    pageClass: 'page',
-  });
+  $('.pages-wrapper').pageSwiper();
 });
 
 Template.Standings.onDestroyed(function () {
+  $('.page-switcher').remove();
 });
