c98441a71567afec1e11f25745d6a282c388864b
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-06-07 22:05:31 +0200

    Show points per bet

--- app/client/templates/user/user.js
+++ app/client/templates/user/user.js
@@ -47,7 +47,7 @@ Template.User.onRendered(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 1500,
+        duration: 2000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
