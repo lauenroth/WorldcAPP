a219c147c63086a88412773c064cefc90bdcdf4a
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2016-06-08 22:21:44 +0200

    Set basic user profile info

--- app/server/main.js
+++ app/server/main.js
@@ -3,3 +3,17 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Accounts.onCreateUser(function(options, user) {
+  if (!user.profile) user.profile = {};
+  if (user.username) {
+    user.profile.name = user.username;
+  } else if (user.services) {
+    if (user.services.facebook) {
+      user.profile.name = user.services.facebook.name;
+    }
+  }
+  user.profile.supportedTeam = 'tbd';
+  user.profile.points = 0;
+  return user;
+});
